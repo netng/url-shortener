40c9336bf72e03707c86c9cb91cdc7b327691bf0
nan <nandangsopyan08@gmail.com>
2022-05-11 22:31:44 +0700

    added spec test for url model

--- spec/models/url_spec.rb
+++ spec/models/url_spec.rb
@@ -1,5 +1,73 @@
 require 'rails_helper'
 
 RSpec.describe Url, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "Validation presence test" do
+    it "Ensure url presence" do
+      url = Url.new(
+        url: nil,
+        shortcode: "1aBc"
+      ).save
+
+      expect(url).to eq(false)
+    end
+
+    it "Ensure shortcode presence" do
+      url = Url.new(
+        url: "google.com",
+        shortcode: nil
+      ).save
+
+      expect(url).to eq(false)
+    end
+  end
+
+  context "Validation length test" do
+    it "Ensure shortcode character length more than 3" do
+      url = Url.new(
+        url: "google.com",
+        shortcode: "1aB"
+      ).save
+
+      expect(url).to eq(false)
+    end
+  end
+
+  context "Validation shortcode format" do
+    it "Ensures shortocde format not valid" do
+      url = Url.new(
+        url: "google.com",
+        shortcode: "aabc?"
+      ).save
+
+      expect(url).to eq(false)
+    end
+
+    it "Ensures shortocde format valid" do
+      url = Url.new(
+        url: "google.com",
+        shortcode: "1aB_"
+      ).save
+
+      expect(url).to eq(true)
+    end
+  end
+
+  context "Validation uniqueness shortcode" do
+    before do
+      Url.new(
+        url: "google.com",
+        shortcode: "1aBc_"
+      ).save
+    end
+
+    it "Ensures shortcode has uniqueness" do
+      url = Url.new(
+        url: "google.com",
+        shortcode: "1aBc_"
+      ).save
+
+      expect(url).to eq(false)
+
+    end
+  end
 end
