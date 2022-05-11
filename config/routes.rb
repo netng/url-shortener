9324f51a81ad6783de0701d537cd9f6efb4143ee
nan <nandangsopyan08@gmail.com>
2022-05-12 00:31:32 +0700

    updating routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :shortens
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  post 'shorten', to: 'shortens#create'
+  get '/:shortcode', to: 'shortens#show'
 end
