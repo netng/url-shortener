646130c6c4404312ffb12b0e3487b5f390af408b
nan <nandangsopyan08@gmail.com>
2022-05-12 02:08:05 +0700

    updating url models

--- app/models/url.rb
+++ app/models/url.rb
@@ -3,8 +3,6 @@ class Url < ApplicationRecord
 
     VALID_SHORTCODE_REGEX = /^[0-9a-zA-Z_]{4,}$/
     validates :shortcode, 
-        presence: true,
         uniqueness: { case_sensitive: true },
-        length: { minimum: 4 },
         format: { with: VALID_SHORTCODE_REGEX, multiline: true }
 end
