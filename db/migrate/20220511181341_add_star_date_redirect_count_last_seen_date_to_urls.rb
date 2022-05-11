f89245b761ab7cda9df7471ea29b6702dd09b44e
nan <nandangsopyan08@gmail.com>
2022-05-12 02:09:24 +0700

    modified migration

--- db/migrate/20220511181341_add_star_date_redirect_count_last_seen_date_to_urls.rb
+++ db/migrate/20220511181341_add_star_date_redirect_count_last_seen_date_to_urls.rb
@@ -1,7 +1,7 @@
 class AddStarDateRedirectCountLastSeenDateToUrls < ActiveRecord::Migration[7.0]
   def change
     add_column :urls, :startDate, :datetime
-    add_column :urls, :redirectCount, :integer
+    add_column :urls, :redirectCount, :integer, default: 0
     add_column :urls, :lastSeenDate, :datetime
   end
 end
