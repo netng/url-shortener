1580688bc39d65195c19e123b75ebcc5b050e2f8
nan <nandangsopyan08@gmail.com>
2022-05-11 22:11:57 +0700

    added validation to url model

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,10 @@
 class Url < ApplicationRecord
+    validates :url, presence: true
+
+    VALID_SHORTCODE_REGEX = /^[0-9a-zA-Z_]{4,}$/
+    validates :shortcode, 
+        presence: true,
+        uniqueness: { case_sensitive: true },
+        length: { minimum: 4 },
+        format: { with: VALID_SHORTCODE_REGEX, multiline: true }
 end
